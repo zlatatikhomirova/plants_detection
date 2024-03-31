172c3c0d8af2a806c84d3ed4879e5438ee21b0ca
Cosgene <tr.gond@gmail.com>
2024-03-31 14:38:40 +0700

    feat: add dataset class

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -8,3 +8,36 @@ from torchvision import transforms
 from typing import Tuple, Dict, List
 
 from utils import find_classes
+
+
+class ImageFolderCustom(Dataset):
+    def __init__(self, targ_dir: str, transform=None) -> None:
+        
+        # Get all image paths
+        self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg")) # have to update this depending on image's extension
+        # Setup transforms
+        self.transform = transform
+        # Create classes and class_to_idx attributes
+        self.classes, self.class_to_idx = find_classes(targ_dir)
+
+    # Make function to load images
+    def load_image(self, index: int) -> Image.Image:
+        "Opens an image via a path and returns it."
+        image_path = self.paths[index]
+        return Image.open(image_path) 
+    
+    def __len__(self) -> int:
+        "Returns the total number of samples."
+        return len(self.paths)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        "Returns one sample of data, data and label (X, y)."
+        img = self.load_image(index)
+        class_name  = self.paths[index].parent.name # expects path in data_folder/class_name/image.jpeg
+        class_idx = self.class_to_idx[class_name]
+
+        # Transform if necessary
+        if self.transform:
+            return self.transform(img), class_idx # return data, label (X, y)
+        else:
+            return img, class_idx # return data, label (X, y)
