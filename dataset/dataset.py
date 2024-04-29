372436e1a0759996d4e0ccb19062a9488b95867b
Cosgene <tr.gond@gmail.com>
2024-04-29 23:04:04 +0700

    feat: change dataset.py and add some utils functions

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,43 +1,32 @@
-import os
-import pathlib
+import numpy as np
+import cv2
 import torch
-
-from PIL import Image
 from torch.utils.data import Dataset
-from torchvision import transforms
-from typing import Tuple, Dict, List
+from utils import augment_data, norm_denoise
 
-from utils import find_classes
+class PlantsDataset(Dataset):
 
-
-class ImageFolderCustom(Dataset):
-    def __init__(self, targ_dir: str, transform=None) -> None:
+    def __init__(self, data,
+                need_preprocessing: bool = True):
         
-        # Get all image paths
-        self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg")) # have to update this depending on image's extension
-        # Setup transforms
-        self.transform = transform
-        # Create classes and class_to_idx attributes
-        self.classes, self.class_to_idx = find_classes(targ_dir)
+        data = augment_data(data)
 
-    # Make function to load images
-    def load_image(self, index: int) -> Image.Image:
-        "Opens an image via a path and returns it."
-        image_path = self.paths[index]
-        return Image.open(image_path) 
-    
-    def __len__(self) -> int:
-        "Returns the total number of samples."
-        return len(self.paths)
-    
-    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
-        "Returns one sample of data, data and label (X, y)."
-        img = self.load_image(index)
-        class_name  = self.paths[index].parent.name # expects path in data_folder/class_name/image.jpeg
-        class_idx = self.class_to_idx[class_name]
+        X = [sample.image for sample in data]
+        if need_preprocessing:
+          X = [norm_denoise(x) for x in X]
+        X = np.array(X)
+        y = np.array(
+                 [sample.mask for sample in data]
+                    )
+        
+        self.X = torch.from_numpy(X).float()
+        self.y = torch.from_numpy(y).float()
 
-        # Transform if necessary
-        if self.transform:
-            return self.transform(img), class_idx # return data, label (X, y)
-        else:
-            return img, class_idx # return data, label (X, y)
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+      return (self.X[index], self.y[index])  
+
+
+              
